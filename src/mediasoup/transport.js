207bdf87e5b828112b848471ca33d4e93c7132d8
Anshul Shakya <anshulshakya18168@gmail.com>
2026-04-27 07:34:35 +0530

    feat: implement createWebRtcTransport utility for mediasoup router initialization

--- src/mediasoup/transport.js
+++ src/mediasoup/transport.js
@@ -1,10 +1,16 @@
 export const createWebRtcTransport = async (router) => {
   const transport = await router.createWebRtcTransport({
-    listenIps: [{ ip: "0.0.0.0", announcedIp: "127.0.0.1" }],
+    listenIps: [
+      { 
+        ip: "0.0.0.0", 
+        announcedIp: process.env.PUBLIC_IP || "127.0.0.1" 
+      }
+    ],
     enableUdp: true,
     enableTcp: true,
     preferUdp: true,
   });
+
 
   return transport;
 };
